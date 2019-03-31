3ec73638666e4dccc05e444d4059330f064b2a4b
Oke Uguru <ondukwe@gmail.com>
2019-03-31 10:10:26 -0400

    added metrics home route

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin')
+const metricsHomeRoutes = require('./routes/metrics-home')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(adminRoutes)
+app.use(metricsHomeRoutes)
 
 
 const port = process.env.PORT || 3000;
