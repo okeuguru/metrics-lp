39537d1e12be88014dce1026a81622e28c178766
Oke Uguru <ondukwe@gmail.com>
2019-03-31 14:42:36 -0400

    added views

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const metricsHomeRoutes = require('./routes/metrics-home')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(adminRoutes)
 app.use(metricsHomeRoutes)
+app.use(adminRoutes)
 
 app.use((req, res, next) => {
     res.status(404).send('<html>Page not found $)$ error</html>')
